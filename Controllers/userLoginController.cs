e585f54ca6692e78d869463344236a3d3f6517ac
Muhammad Arshan <71931953+arsm10@users.noreply.github.com>
2020-10-13 13:37:00 +0200

    files

---
+++ Controllers/userLoginController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ECommerceWebsite.Models;
+using System.Data.SqlClient;
+
+namespace ECommerceWebsite.Controllers
+{
+    public class userLoginController : Controller
+    {
+        SqlConnection con = new SqlConnection();
+        SqlCommand cmd = new SqlCommand();
+        SqlDataReader sdr;
+
+        // GET: userLogin
+        [HttpGet]
+        public ActionResult userAccess()
+        {
+            return View();
+        }
+
+        void connectionString() 
+        {
+            con.ConnectionString = "data source= LAPTOP-CEORD1LI; database= ECommerceDatabase; integrated security= SSPI;";
+        }
+
+        [HttpPost]
+        public ActionResult userVerification(userLogin uL) 
+        {
+            connectionString();
+            con.Open();
+            cmd.Connection = con;
+            cmd.CommandText = "select * from userLoginTable where Username='" + uL.username + "' and Password='" + uL.password + "'";
+            sdr = cmd.ExecuteReader();
+            if (sdr.Read())
+            {
+                con.Close();
+                return View("AccessGrant");
+            }
+            else 
+            {
+                con.Close();
+                return View("Error");
+            }
+        }
+    }
+}
---
+++ Models/userLogin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ECommerceWebsite.Models
+{
+    public class userLogin
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
